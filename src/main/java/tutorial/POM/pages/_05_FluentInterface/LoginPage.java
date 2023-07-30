b0e537d9bc35200133e037f2483d9bc93c32bbed
“Rajat <rajatvermaa95@gmail.com>
2023-07-30 18:26:35 +0530

    POM Implementation (Fluent Interface)

---
+++ src/main/java/tutorial/POM/pages/_05_FluentInterface/LoginPage.java
@@ -0,0 +1,94 @@
+/**
+ * Author: Rajat Verma
+ * Email: rajatvermaa95@gmail.com
+ * GitHub Profile: https://github.com/rajatt95
+ * GitHub Page: https://rajatt95.github.io/
+ * LinkedIn: https://www.linkedin.com/in/rajatt95/
+ * Topmate: https://topmate.io/rajatt95
+ * Telegram: https://t.me/rajatt95
+ * Instagram: https://www.instagram.com/rajattvermaa95/
+ * YouTube: https://www.youtube.com/@rajatt95
+ * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+ */
+
+/***************************************************/
+
+package tutorial.POM.pages._05_FluentInterface;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Represents the LoginPage of the web application.
+ * This class contains web elements declaration and methods to interact with the elements on the Login page.
+ */
+public class LoginPage {
+
+    WebDriver driver;
+
+    /**
+     * Constructor for the LoginPage class.
+     * Initializes the WebDriver.
+     *
+     * @param driver The WebDriver instance to be used for interacting with the web application.
+     */
+    public LoginPage(WebDriver driver){
+        // RHS driver - coming from the test class
+            // When we are creating the object of this class
+                // Constructor of this class gets called
+        // LHS driver - defined in this class
+        this.driver = driver;
+    }
+
+    /***************************************************************************************/
+    // Data Members | Properties
+    // Web Elements Declaration
+    /***************************************************************************************/
+
+
+    // Without Page Factories
+    private By textBox_Username = By.xpath("//*[@id=\"user-name\"]");
+    private By textBox_Password = By.xpath("//input[@placeholder='Password']");
+    private By button_Login = By.id("login-button");
+
+    /***************************************************************************************/
+    // Methods | Tasks | Functionality | Behavior
+    // Operations on the Web Elements
+    /***************************************************************************************/
+
+    /**
+     * Fills the username field with the provided username.
+     *
+     * @param username The username to be filled in the username field.
+     */
+    public LoginPage fill_TextBox_Username(String username){
+        driver.findElement(textBox_Username).sendKeys(username);
+        // This is the example of BUILDER PATTERN
+            // After this operation, User remains on the same page
+        return this;
+    }
+
+    /**
+     * Fills the password field with the provided password.
+     *
+     * @param password The password to be filled in the password field.
+     */
+    public LoginPage fill_TextBox_Password(String password){
+        driver.findElement(textBox_Password).sendKeys(password);
+        // This is the example of BUILDER PATTERN
+            // After this operation, User remains on the same page
+        return this;
+    }
+
+    /**
+     * Clicks the login button.
+     */
+    public HomePage click_Button_Login(){
+        driver.findElement(button_Login).click();
+        // This is the example of FLUENT INTERFACE
+            // After this operation, User navigates to the HomePage
+        return new HomePage(driver);
+    }
+
+
+}
---
+++ src/test/java/tutorial/POM/tests/Test_05_Login_POM_FluentInterface.java
@@ -0,0 +1,76 @@
+/**
+ * This is a test class that contains test cases for the login functionality using the
+ * Page Object Model (POM) without Page Factories.
+ *
+ * Author: Rajat Verma
+ * Email: rajatvermaa95@gmail.com
+ * GitHub Profile: https://github.com/rajatt95
+ * GitHub Page: https://rajatt95.github.io/
+ * LinkedIn: https://www.linkedin.com/in/rajatt95/
+ * Topmate: https://topmate.io/rajatt95
+ * Telegram: https://t.me/rajatt95
+ * Instagram: https://www.instagram.com/rajattvermaa95/
+ * YouTube: https://www.youtube.com/@rajatt95
+ * WhatsApp: https://chat.whatsapp.com/DVRP0ViD0YwELUKF2XMJHy
+ */
+
+/***************************************************/
+
+
+package tutorial.POM.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tutorial.POM.base.BaseTest;
+import tutorial.POM.pages._05_FluentInterface.HomePage;
+import tutorial.POM.pages._05_FluentInterface.LoginPage;
+
+
+/**
+ * This is a test class that contains test cases for the login functionality using the
+ * Page Object Model (POM)
+ * 1. Without PAGE FACTORIES.
+ * 2. BUILDER PATTERN
+ * 3. FLUENT INTERFACE
+ */
+
+public class Test_05_Login_POM_FluentInterface extends BaseTest {
+
+    // Test_05_Login_POM_FluentInterface extends BaseTest - This is the example of INHERITANCE (OOPS Concept) - Single-level inheritance
+        // It is a concept that acquires the properties and behaviors from Parent/Base/Super class to Child/Derived/Sub classes
+
+    /**
+     * Test case to verify login functionality with valid credentials.
+     * It navigates to the login page, fills in the username and password, and clicks the login button.
+     * After successful login, it checks the presence of the Twitter icon in the footer and verifies the footer message.
+     */
+    @Test
+    public void test_Login_Functionality_Valid_Credentials(){
+
+        getDriver().get("https://www.saucedemo.com/");
+
+        LoginPage loginPage = new LoginPage(getDriver());
+        // This is the example of BUILDER PATTERN
+            // After this operation, User remains on the same page
+
+        // This is the example of FLUENT INTERFACE
+            // After clicking on the Login button, User navigates to the HomePage
+        HomePage homePage = loginPage.
+                fill_TextBox_Username("standard_user").
+                fill_TextBox_Password("secret_sauce").
+                click_Button_Login();
+
+        // Assert.assertEquals() -
+            // Boolean values as Actual and Expected
+            // String values as Actual and Expected
+        // This is the example of POLYMORPHISM (OOPS Concept) - Compile Time (Method Overloading)
+            // It allows us to perform a single action in different ways.
+        Assert.assertEquals(homePage.get_Element_Icon_Twitter().isDisplayed(), true,
+                "Assertion for Twitter icon presence in the Footer");
+
+        String msg_FooterText_Actual = homePage.get_Element_Msg_Footer().getText();
+        String msg_FooterText_Expected = "© 2023 Sauce Labs. All Rights Reserved. Terms of Service | Privacy Policy";
+        Assert.assertEquals(msg_FooterText_Actual, msg_FooterText_Expected,
+                "Assertion for message present in the Footer");
+    }
+}
